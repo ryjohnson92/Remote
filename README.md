b9fe578519872af3ea41313ca995482350ea2a7f
Ryan <ryan.johnson@gtl.net>
2024-08-01 08:45:49 -0400

    adding mysql

--- README.md
+++ README.md
@@ -6,15 +6,60 @@ This module will aid in properly executing remote SSH commands against remote ho
 
 ## Usage/Examples
 
-### Simple
+### Remote.shell
+#### Simple
 ```python
 from Remote.shell import cmd
 with cmd('ls',server='server.my.com',keyfile='./myrootkey',user='root') as cmd:
     print(cmd)
 ```
-### Iteration
+#### Iteration
 ```python
+from Remote.shell import cmd
 with cmd('ls',server='server.my.com',listen=True,keyfile='./myrootkey',user='root') as cmd:
     for result in cmd:
         print(result)
+```
+### Remote.mysql
+
+#### Simple
+The following uses the subclass init function to pass credentials
+```python
+from Remote.mysql import connection
+class my_query(connection):
+    def __init__(self, payload):
+        super().__init__(payload,'192.168.255.255','your_user','your_password')
+    def query(self):
+        self.cur.execute('select * from MyDB.MyTable limit 10')
+        return self.cur.fetchall()
+
+with my_query('') as results:
+    print(results)
+```
+#### Supercession
+The following examples use a child class to assign credentials, if you are using this multiple
+times or for multiple connection / query types this is often the best usage.
+```python
+from Remote.mysql import connection
+
+class my_local_connection(connection):
+    def __init__(self, payload):
+        super().__init__(payload,'192.168.255.255','your_user','your_password')
+    pass
+
+class my_query(my_local_connection):
+    def query(self):
+        self.cur.execute('select * from MyDB.MyTable limit 10')
+        return self.cur.fetchall()
+
+class my_second_query(my_local_connection):
+    def query(self):
+        self.cur.execute('select * from MyDB.MySecondTable limit 10')
+        return self.cur.fetchall()
+
+with my_query('') as results:
+    print(results)
+
+with my_second_query('') as results:
+    print(results)
 ```
---
+++ mysql.py
@@ -0,0 +1,83 @@
+import mysql.connector
+
+class connection:
+    """
+        Handles connecting to mysql database
+    """
+    def __init__(self,payload,host:str='',user:str='',password:str='',port:int=3306):
+        self.conn = mysql.connector.connect(
+            host=host ,
+            user=user,
+            password=password,
+            port= port
+        )
+        self.conn.autocommit = True
+        self.cur = self.conn.cursor()
+        self.payload = payload
+        
+    def __enter__(self):
+        return self.query()
+    
+    def __exit__(self,a,b,c):
+        try: 
+            self.cur.close()
+            self.conn.close()
+        except: pass
+
+    @staticmethod
+    def help():
+        print("""
+MYSQL Connection Module! 
+==============================================================================================
+A context manager implementation of mysql-connector!
+==============================================================================================
+Consider the following usage example!
+______________________________________________________________________________________________
+
+Simple usage
+The following uses the subclass init function to pass credentials
+______________________________________________________________________________________________
+
+from Telmate.mysql import connection
+class my_query(connection):
+    def __init__(self, payload):
+        super().__init__(payload,'192.168.255.255','your_user','your_password')
+    def query(self):
+        self.cur.execute('select * from MyDB.MyTable limit 10')
+        return self.cur.fetchall()
+
+with my_query('') as results:
+    print(results)
+______________________________________________________________________________________________    
+
+Supercession
+The following examples use a child class to assign credentials, if you are using this multiple
+times or for multiple connection / query types this is often the best usage.
+______________________________________________________________________________________________
+
+from Telmate.mysql import connection
+
+class my_local_connection(connection):
+    def __init__(self, payload):
+        super().__init__(payload,'192.168.255.255','your_user','your_password')
+    pass
+
+class my_query(my_local_connection):
+    def query(self):
+        self.cur.execute('select * from MyDB.MyTable limit 10')
+        return self.cur.fetchall()
+
+class my_second_query(my_local_connection):
+    def query(self):
+        self.cur.execute('select * from MyDB.MySecondTable limit 10')
+        return self.cur.fetchall()
+
+with my_query('') as results:
+    print(results)
+
+with my_second_query('') as results:
+    print(results)
+______________________________________________________________________________________________            
+""")
+
+        pass
